d36607e055f095b2e2bec2111c4b45d1ea3ab0e9
Salma Khaled <77202068+SalmaKhaled879@users.noreply.github.com>
2025-02-01 03:40:51 +0200

    connect to db

--- Api/src/server.js
+++ Api/src/server.js
@@ -1,5 +1,7 @@
+
 const app= require('./app');
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //Load environment variables
 dotenv.config();
@@ -9,3 +11,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+connectDB(); // Connect to MongoDB
