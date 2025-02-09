af8a10918fb13cb638b04cc988aa64d5d4ec6700
Salma Khaled <77202068+SalmaKhaled879@users.noreply.github.com>
2025-02-09 18:35:43 +0200

    modify updateUserProfile controller

--- Api/src/controllers/userController.js
+++ Api/src/controllers/userController.js
@@ -17,13 +17,34 @@ export const getUserProfile = async (req, res) => {
   //update user profile
 export const updateUserProfile = async (req, res) => {
   try {
-    const updates = req.body;
-    const user = await User.findByIdAndUpdate(req.user.id, updates, { new: true }).select('-password');
-    res.json(user);
-  } catch (err) {
+    const {name, email, password, locumProfile, hospitalProfile, role }= req.body;
+    const user = await User.findById(req.user.id);
+    //const {password: pass, ...safeUser}= user.toObject();
+    if(!user){
+      return res.status(404).json({msg: 'User not found'});
+    }
+    if (role && role !== user.role) {
+      return res.status(403).json({ msg: 'Role modification not allowed' });
+    }
+    if(name) user.name = name;  
+    if(email) user.email = email;
+    if(password){
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+    }
+    if (user.role === 'locum' && locumProfile) {
+      user.locumProfile = { ...user.locumProfile, ...locumProfile };
+    }
+    if (user.role === 'hospital' && hospitalProfile) {
+      user.hospitalProfile = { ...user.hospitalProfile, ...hospitalProfile };
+    }
+    await user.save();
+    res.json({msg: 'Profile updated successfully', user: safeUser});
+  } catch (err) {  
     res.status(500).json({ msg: 'Server error' });
   }
 };
+
 
 //delete user profile
 export const deleteUserProfile = async (req, res) => {
