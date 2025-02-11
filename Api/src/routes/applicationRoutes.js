9702f9e69ddd4c1755440f7c96ae7d8058bd146b
Salma Khaled <77202068+SalmaKhaled879@users.noreply.github.com>
2025-02-11 13:08:49 +0200

    add updateApplicationStatus route to applicationRoutes

--- Api/src/routes/applicationRoutes.js
+++ Api/src/routes/applicationRoutes.js
@@ -9,5 +9,6 @@ router.get('/', authUser, getAllApplications);
 router.get('/:id', authUser, getApplicationById);
 router.put('/:id', authUser, updateApplicationById);
 router.delete('/:id', authUser, deleteApplicationById);
+router.put('/:id/status', authUser,authRole('hospital'), updateApplicationStatus); //does status need to be in the body?
 
 export default router;
